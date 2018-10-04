c6a6aa50af787a36977afd1000c0e1d381cab57f
Rünno Ruul <rynno.ruul@telia.ee>
2018-10-04 17:43:33 +0300

    error

--- auth_service/src/main/java/eu/babywatcher/AuthServiceApplication.java
+++ auth_service/src/main/java/eu/babywatcher/AuthServiceApplication.java
@@ -59,7 +59,7 @@ public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**").permitAll().anyRequest()
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll().anyRequest()
 				.authenticated().and().exceptionHandling()
 				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
 				.logoutSuccessUrl("/").permitAll().and().csrf()
