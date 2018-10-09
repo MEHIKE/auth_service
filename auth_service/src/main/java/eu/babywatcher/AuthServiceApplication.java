6afc4e2edca519ebb3e894c18b61fa1887a63c0b
Rünno Ruul <rynno.ruul@telia.ee>
2018-10-09 23:01:07 +0300

    c

--- auth_service/src/main/java/eu/babywatcher/AuthServiceApplication.java
+++ auth_service/src/main/java/eu/babywatcher/AuthServiceApplication.java
@@ -11,21 +11,32 @@ import javax.servlet.Filter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.undertow.UndertowServletWebServerFactory;
+import org.springframework.boot.web.server.ErrorPage;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
@@ -44,6 +55,7 @@ import org.springframework.web.filter.CompositeFilter;
 @EnableAuthorizationServer
 @SpringBootApplication
 @Order(200)
+//@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -59,8 +71,12 @@ public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		// @formatter:off
-		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**", "/error**","/uaa/login**","/uaa/error**","/uaa**").permitAll().anyRequest()
-				.authenticated().and().exceptionHandling()
+		http.antMatcher("/**")
+			.authorizeRequests()
+			.antMatchers("/", "/login**", "/webjars/**", "/error**","/uaa/login**","/uaa/error**","/uaa**")
+			.permitAll()
+			.anyRequest().authenticated()
+			.and().exceptionHandling()
 				.authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/")).and().logout()
 				.logoutSuccessUrl("/").permitAll().and().csrf()
 				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()).and()
@@ -95,7 +111,59 @@ public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
 	public ClientResources github() {
 		return new ClientResources();
 	}
+	/*@Bean
+	@ConfigurationProperties("github.client")
+	public AuthorizationCodeResourceDetails github() {
+		return new AuthorizationCodeResourceDetails();
+	}*/
 
+	@RequestMapping("/unauthenticated")
+	public String unauthenticated() {
+	  return "redirect:/?error=true";
+	}
+
+	/*@Configuration
+	public class ServletCustomizer {
+	  @Bean
+	  public EmbeddedServletContainerCustomizer customizer() {
+	    return container -> {
+	      container.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
+	    };
+	  }
+	}*/
+	
+	@Bean
+    public ConfigurableServletWebServerFactory webServerFactory() {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        factory.addErrorPages(new ErrorPage(HttpStatus.UNAUTHORIZED, "/unauthenticated"));
+        return factory;
+    }
+
+	@Bean
+	public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+	  return map -> {
+	    String url = (String) map.get("organizations_url");
+	    @SuppressWarnings("unchecked")
+	    List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+	    if (orgs.stream()
+	        .anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+	      return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+	    }
+	    throw new BadCredentialsException("Not in Spring Projects origanization");
+	  };
+	}
+	
+	@Bean
+	public OAuth2RestTemplate oauth2RestTemplate(OAuth2ProtectedResourceDetails resource, OAuth2ClientContext context) {
+		return new OAuth2RestTemplate(resource, context);
+	}
+	
+/*	@Bean
+	@ConfigurationProperties("github.resource")
+	public ResourceServerProperties githubResource() {
+		return new ResourceServerProperties();
+	}*/
+	
 	@Bean
 	@ConfigurationProperties("facebook")
 	public ClientResources facebook() {
@@ -110,7 +178,27 @@ public class AuthServiceApplication extends WebSecurityConfigurerAdapter {
 		filter.setFilters(filters);
 		return filter;
 	}
-
+	/*private Filter ssoFilter() {
+		  OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+		  OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+		  facebookFilter.setRestTemplate(facebookTemplate);
+		  UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
+		  tokenServices.setRestTemplate(facebookTemplate);
+		  facebookFilter.setTokenServices(tokenServices);
+		  return facebookFilter;
+	}*/
+	
+	/*@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+	  return new AuthorizationCodeResourceDetails();
+	}
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+	  return new ResourceServerProperties();
+	}*/
+	
 	private Filter ssoFilter(ClientResources client, String path) {
 		OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(
 				path);
---
+++ auth_service/src/main/java/eu/babywatcher/ResourceServerConfigurerAdapter.java
@@ -0,0 +1,19 @@
+package eu.babywatcher;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer
+@Order(199)
+abstract class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+  @Override
+  public void configure(HttpSecurity http) throws Exception {
+    http
+      .antMatcher("/me")
+      .authorizeRequests().anyRequest().authenticated();
+  }
+}
